b8ff172cf60db37b4b2eb87e3d405c2dc780eaca
ecarbo <enriquecarbo@tutanota.com>
2024-08-25 11:01:43 -0300

    Create CartReducer

--- src/components/shadcn/Carrito.tsx
+++ src/components/shadcn/Carrito.tsx
@@ -9,54 +9,7 @@ import {
 } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
 import { availableProducts, type Product } from "@/assets/data/products";
-import type { CartAction, CartState } from "@/types";
-
-
-function cartReducer(state: CartState, action: CartAction): CartState {
-  let newState: CartState;
-  switch (action.type) {
-    case 'ADD_ITEM': {
-      const existingItem = state.items.find(item => item.id === action.payload.id);
-      let newItems;
-      if (existingItem) {
-        newItems = state.items.map(item =>
-          item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      } else {
-        newItems = [...state.items, { ...action.payload, quantity: 1 }];
-      }
-      const newTotal = newItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
-      newState = { items: newItems, total: newTotal };
-      break;
-    }
-    case 'REMOVE_ITEM': {
-      const existingItem = state.items.find(item => item.id === action.payload);
-      let newItems;
-      if (existingItem && existingItem.quantity > 1) {
-        newItems = state.items.map(item =>
-          item.id === action.payload ? { ...item, quantity: item.quantity - 1 } : item
-        );
-      } else {
-        newItems = state.items.filter(item => item.id !== action.payload);
-      }
-      const newTotal = newItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
-      newState = { items: newItems, total: newTotal };
-      break;
-    }
-    case 'CLEAR_CART':
-      newState = { items: [], total: 0 };
-      break;
-    case 'LOAD_CART':
-      newState = action.payload;
-      break;
-    default:
-      return state;
-  }
-  
-  // Guardar el nuevo estado en localStorage
-  localStorage.setItem('cart', JSON.stringify(newState));
-  return newState;
-}
+import cartReducer from "@/components/reducer/CartReducer"
 
 function Carrito() {
   const [cartState, dispatch] = useReducer(cartReducer, { items: [], total: 0 });
