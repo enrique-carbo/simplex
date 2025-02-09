12faf2f07ce334dc2eadaa048962133f7f11d0e2
ecarbo <enriquecarbo@tutanota.com>
2025-02-09 17:26:31 -0300

    Add TikTok to social

--- src/navigation.js
+++ src/navigation.js
@@ -102,7 +102,7 @@ export const footerData = {
     },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'TikTok', icon: 'tabler:brand-tiktok', href: 'https://www.tiktok.com/@simplex.ar' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') }
