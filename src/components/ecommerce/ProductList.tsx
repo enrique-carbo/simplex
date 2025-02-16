99701a77518645d118744efca5b6c73f3e341eb3
ecarbo <enriquecarbo@tutanota.com>
2025-02-16 10:00:41 -0300

    Making some changes

--- src/components/ecommerce/ProductList.tsx
+++ src/components/ecommerce/ProductList.tsx
@@ -35,7 +35,9 @@ const ProductList: React.FC<ProductListProps> = ({ category }) => {
             <CardContent>
               <img src={product.image} alt={product.name} className="w-full h-48 object-contain" />
               <div className="p-4">
-                <h2 className="text-xl font-semibold mb-2">Art-{product.item}: {product.name}</h2>
+                <h2 className="text-xl font-medium mb-2">Art-{product.item}: 
+                <span className='font-semibold'> {product.name}</span>
+                </h2>
                 <div className="flex">
                   <p className="text-gray-900 dark:text-white text-xl font-medium">${product.discountedPrice.toFixed(2)}</p>
                   {product.discountedPrice < product.listPrice && (
--- src/components/ecommerce/SalesList.tsx
+++ src/components/ecommerce/SalesList.tsx
@@ -1,34 +1,53 @@
 import React from 'react';
 import { availableSales } from '@/assets/data/sales/sales';
 import { Card, CardContent } from '@/components/ui/card';
-
+import { Badge } from '@/components/ui/badge';
 
 const SalesList: React.FC = () => {
-  
-
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-10 mb-10">
-      {availableSales.map((sale) => (
-        <Card 
-        key={sale.id}
-        className="p-4 rounded-lg shadow-md overflow-hidden cursor-pointer transition-transform hover:scale-105"
-        >
-        <CardContent>  
-        
-          <img src={sale.image} alt={sale.name} className="w-full h-48 object-contain" />
-          <div className="p-4">
-            <h2 className="text-xl font-semibold mb-2">OFERTA: {sale.name}</h2>
-            <p>${sale.price.toFixed(2)}</p>
-            {sale.inStock ? (
-                <p className="text-green-500">Disponible</p>
-              ) : (
-                <p className="text-red-500">Sin stock</p>
+    <div>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-10">
+        {availableSales.map((sale) => {
+          const discountPercentage = (
+            ((sale.listPrice - sale.discountedPrice) / sale.listPrice) * 100
+          ).toFixed(0);
+
+          return (
+            <Card
+              key={sale.id}
+              className="p-4 rounded-lg shadow-md overflow-hidden cursor-pointer transition-transform hover:scale-105 relative"
+            >
+              {sale.discountedPrice < sale.listPrice && (
+                <Badge className="absolute top-2 left-2 text-red-500 bg-white border-red-500 dark:text-white dark:bg-red-500 z-10">
+                  {discountPercentage}% OFF
+                </Badge>
               )}
-          </div>
-        
-        </CardContent>
-        </Card>
-      ))}
+
+              <CardContent>
+                <img src={sale.image} alt={sale.name} className="w-full h-48 object-contain" />
+                <div className="p-4">
+                  <h2 className="text-xl font-medium mb-2">Art-{sale.item}:  
+                  <span className='font-semibold'> {sale.name}</span>
+                  </h2>
+                  <div className="flex">
+                    <p className="text-gray-900 dark:text-white text-xl font-medium">${sale.discountedPrice.toFixed(2)}</p>
+                    {sale.discountedPrice < sale.listPrice && (
+                      <p className="ml-4 line-through text-gray-500">${sale.listPrice.toFixed(2)}</p>
+                    )}
+                  </div>
+                  {sale.inStock ? (
+                    <p className="text-green-500">Disponible</p>
+                  ) : (
+                    <p className="text-red-500">Sin stock</p>
+                  )}
+                </div>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
+
     </div>
   );
 };
